da1bb3d6ca9f9b8508644b3009bc570204bec97d
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-12-05 14:50:14 +0100

    create filterset to  Room

--- backend/hotel_management/views.py
+++ backend/hotel_management/views.py
@@ -1,8 +1,11 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import Hotel, Location, Room
 from .serializers import HotelModelSerializer, LocationModelSerializer, RoomModelSerializer
+from .filters import RoomFilters
+from .permissions import IsAdminOrReadOnly
 
 from rest_framework import viewsets
-from .permissions import IsAdminOrReadOnly
 
 
 class HotelModelViewSet(viewsets.ModelViewSet):
@@ -15,6 +18,8 @@ class RoomModelViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomModelSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RoomFilters
 
 
 class LocationModelViewSet(viewsets.ModelViewSet):
