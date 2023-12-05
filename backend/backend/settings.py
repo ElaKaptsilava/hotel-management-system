0f3fba50230ec147f55f11914f8cb18ba7997a4b
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-12-05 13:46:32 +0100

    create models in hotel_managemrnt app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'django_filters',
+    'phonenumber_field',
+
+    'hotel_management'
 
 ]
 
---
+++ backend/hotel_management/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=250)
+    location = models.OneToOneField('Location', on_delete=models.CASCADE)
+    description = models.TextField()
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    street = models.CharField(max_length=250)
+    state = models.CharField(max_length=250)
+
+
+class Room(models.Model):
+    class Status(models.TextChoices):
+        available = 'Available'
+        not_available = 'NotAvailable'
+        reserved = 'Reserved'
+        occupied = 'Occupied'
+
+    room_number = models.IntegerField(unique=True)
+    status = models.CharField(max_length=15, choices=Status.choices, default=Status.available)
+    prise_per_day = models.DecimalField(max_digits=5, decimal_places=2)
+    phone_number = PhoneNumberField(blank=True)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
