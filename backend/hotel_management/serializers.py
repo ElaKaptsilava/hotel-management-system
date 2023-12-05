c8c633d11fb479aa9bce8c21b451df3c77615342
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-12-05 13:51:49 +0100

    create serializers to models

---
+++ backend/hotel_management/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Hotel, Room, Location
+
+
+class HotelModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = '__all__'
+
+
+class RoomModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+
+class LocationModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
