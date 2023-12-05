4b58e22a6935e9b60a83f0d9f20a2d8682a7449f
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2023-12-05 14:21:07 +0100

    create permissions IsAdminOReadOnly, include to models

---
+++ backend/hotel_management/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admin of an object to edit it.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and request.user.is_staff
+        )
--- backend/hotel_management/views.py
+++ backend/hotel_management/views.py
@@ -2,18 +2,22 @@ from .models import Hotel, Location, Room
 from .serializers import HotelModelSerializer, LocationModelSerializer, RoomModelSerializer
 
 from rest_framework import viewsets
+from .permissions import IsAdminOrReadOnly
 
 
 class HotelModelViewSet(viewsets.ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class RoomModelViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class LocationModelViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationModelSerializer
+    permission_classes = [IsAdminOrReadOnly]
