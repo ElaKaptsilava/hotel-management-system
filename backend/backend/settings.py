c81fb65980fa11a47eda6e3f21d06b1f102e119e
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-04-02 12:29:33 +0200

    protecte to secrete_key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,7 @@ import os
 
 import sentry_sdk
 from sentry_sdk.crons import monitor
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "django-insecure-=%tv!*5xy)e#$yhj85shoxoxzn=a^i76i&99q45biy!p(v*yrz"
-)
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
